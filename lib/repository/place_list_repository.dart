c736352cc48941edf9b7a31ecc131c15d4f7c5cb
flutterWithChris <christian@noidbotanicals.com>
2022-09-19 14:35:39 -0400

    Places Subcollection & Add Place to Firestore

--- lib/repository/place_list_repository.dart
+++ lib/repository/place_list_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:leggo/model/place.dart';
 import 'package:leggo/model/place_list.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:rxdart/rxdart.dart';
@@ -18,6 +19,42 @@ class PlaceListRepository {
         .collection('place_lists')
         .doc(placeList.name)
         .set(placeList.toDocument());
+  }
+
+  Future<void> addPlaceToList(Place place, PlaceList placeList) async {
+    final auth.User firebaseUser = auth.FirebaseAuth.instance.currentUser!;
+    _firebaseFirestore
+        .collection('users')
+        .doc(firebaseUser.uid)
+        .collection('place_lists')
+        .doc(placeList.name)
+        .collection('places')
+        .doc(place.name)
+        .set(place.toDocument());
+  }
+
+  Stream<Place> getPlaces(PlaceList placeList) {
+    final auth.User firebaseUser = auth.FirebaseAuth.instance.currentUser!;
+
+    return _firebaseFirestore
+        .collection('users')
+        .doc(firebaseUser.uid)
+        .collection('place_lists')
+        .doc(placeList.name)
+        .collection('places')
+        .snapshots()
+        .switchMap(((snapshot) {
+      final references = snapshot.docs;
+      return MergeStream(references.map((snap) => _firebaseFirestore
+          .collection('users')
+          .doc(firebaseUser.uid)
+          .collection('place_lists')
+          .doc(placeList.name)
+          .collection('places')
+          .doc(snap.id)
+          .snapshots()
+          .map((snap) => Place.fromSnapshot(snap))));
+    }));
   }
 
   Stream<PlaceList> getPlaceLists() {
